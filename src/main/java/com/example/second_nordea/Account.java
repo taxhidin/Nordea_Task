9fd67d383b529b03309f346cf160b80405eafd4c
taxhidin <taxhidin1@gmail.com>
2022-09-03 21:33:33 +0200

    Final Code

--- src/main/java/com/example/second_nordea/Account.java
+++ src/main/java/com/example/second_nordea/Account.java
@@ -2,15 +2,9 @@ package com.example.second_nordea;
 
 public class Account {
 
-    private static John john;
 
     private double balance;
 
-    public static void testing_method(String first_name, String last_name, double initial_balance_John_a, boolean value) {
-
-        john = new John(first_name, last_name, initial_balance_John_a, value);
-
-    }
 
     public double getBalance() {
 
--- src/main/java/com/example/second_nordea/SecondNordeaApplication.java
+++ src/main/java/com/example/second_nordea/SecondNordeaApplication.java
@@ -15,72 +15,8 @@ public class SecondNordeaApplication {
     public static void main(String[] args) {
         SpringApplication.run(SecondNordeaApplication.class, args);
 
-        ObjectMapper mapper = new ObjectMapper();
-
-        try {
-            John John_characteristics = mapper.readValue(new File("data/John_Detail.json"), John.class);
-            Account account_1 = new Account(John_characteristics.getInitial_balance_John());
-
-            System.out.println("Enter deposit amount for John: ");
-            deposit_amount_John = input.nextDouble();
-            input.nextLine();
-
-            account_1.credit(deposit_amount_John);
-            Address_John address_john = John_characteristics.getAddress();
-
-
-            Bob Bob_Characteristics = mapper.readValue(new File("data/Bob_Detail.json"), Bob.class);
-            Account account_2 = new Account(Bob_Characteristics.getInitial_balance_Bob());
-
-            System.out.printf("%s", "Enter deposit amount for Bob: ");
-            double depoist_amount_Bob = input.nextDouble();
-            input.close();
-
-            account_2.credit(depoist_amount_Bob);
-            Address_Bob address_bob = Bob_Characteristics.getAddress();
-
-            System.out.println("First name and last name: \n" + John_characteristics.getFirstName() + " " + John_characteristics.getLastName() + "\n");
-            System.out.println("John id: \n" + John_characteristics.getId() + "\n");
-            System.out.println("John account status: \n" + John_characteristics.isAccount_status() + "\n");
-            System.out.println("John initial balance: \n" + John_characteristics.getInitial_balance_John() + "\n");
-            System.out.printf("John address Street: %s, City: %s, State: %s, Zip: %d, Country: %s \n"
-                    , address_john.getStreet()
-                    , address_john.getCity()
-                    , address_john.getState()
-                    , address_john.getZip()
-                    , address_john.getCountry());
-            System.out.println("John languages: ");
-            for (String Lang_John : John_characteristics.getLanguages()){
-
-                System.out.print(Lang_John+"  ");
-
-            }
-            System.out.println("\n");
-            System.out.printf("John balance: $%.5f\n\n", account_1.getBalance());
-
-
-
-            System.out.printf("First name and last name:\n%s %s\n", Bob_Characteristics.getFirstName(), Bob_Characteristics.getLastName());
-            System.out.println("Bob id: \n" + Bob_Characteristics.getId() + "\n");
-            System.out.println("Bob account status: \n" + Bob_Characteristics.isAccount_status() + "\n");
-            System.out.println("Bob initial balance: \n" + Bob_Characteristics.getInitial_balance_Bob() + "\n");
-            System.out.printf("Bob address Street: %s, City: %s, State: %s, Zip: %d, Country: %s \n"
-                    , address_bob.getStreet()
-                    , address_bob.getCity()
-                    , address_bob.getState()
-                    , address_bob.getZip()
-                    , address_bob.getCountry());
-            System.out.println("Bob languages: ");
-            for(String Lang_Bob : Bob_Characteristics.getLanguages()){
-                System.out.printf("%s  ", Lang_Bob);
-            }
-            System.out.println("\n");
-            System.out.printf("Bob balance: $%.5f\n", account_2.getBalance());
-
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-
+        calculation calculation_First = new calculation();
+        calculation_First.calculation_all();
 
     }
 
